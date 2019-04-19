c8a6e43320a3761d2bf25e7148f523c4a48f7d61
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-18 17:47:56 -0700

    Can get images

--- api/server.js
+++ api/server.js
@@ -18,43 +18,38 @@ server.use("/api/players", playersRouter);
 server.use("/api/teams", teamsRouter);
 server.use("/api/users", usersRouter);
 
-server.get("/", async (req, res) => {
-  res.status(200).json({ api: "NBA" });
-});
-
-
 //////////////////////////////
 // Endpoint that makes a call to the sum.py python file
 const { PythonShell } = require("python-shell");
 
-server.get("/sum", callSUM);
+// server.get("/sum", callSUM);
 
-function callSUM(req, res) {
-  let options = {
-    mode: "text",
-    pythonPath: "python",
-    pythonOptions: ["-u"],
-    scriptPath: "",
+// function callSUM(req, res) {
+//   let options = {
+//     mode: "text",
+//     pythonPath: "python",
+//     pythonOptions: ["-u"],
+//     scriptPath: "",
 
-    args: [
-      parseInt(req.query.a),
-      parseInt(req.query.b),
-      parseInt(req.query.c),
-      parseInt(req.query.d)
-    ]
-  };
-  let pySUM = new PythonShell("sum.py", options);
-  pySUM.send(JSON.stringify(options.args));
-  pySUM.on("message", message => {
-    res.send(message);
-  });
+//     args: [
+//       parseInt(req.query.a),
+//       parseInt(req.query.b),
+//       parseInt(req.query.c),
+//       parseInt(req.query.d)
+//     ]
+//   };
+//   let pySUM = new PythonShell("sum.py", options);
+//   pySUM.send(JSON.stringify(options.args));
+//   pySUM.on("message", message => {
+//     res.send(message);
+//   });
 
-  pySUM.end(function(err) {
-    if (err) {
-      throw err;
-    }
-  });
-}
+//   pySUM.end(function(err) {
+//     if (err) {
+//       throw err;
+//     }
+//   });
+// }
 ////////////////////////////////
 
 //////////////////////////////
@@ -68,11 +63,11 @@ function callSUM(req, res) {
     mode: "text",
     pythonPath: "python3",
     pythonOptions: ["-u"],
-    scriptPath: "./python",
+    scriptPath: "./python"
   };
 
   let pySurvival = new PythonShell("nba_survival.py", options);
-  pySurvival.send(JSON.stringify(req.body) );
+  pySurvival.send(JSON.stringify(req.body));
   pySurvival.on("message", message => {
     res.send(message);
     // res.sendFile(message)
@@ -86,27 +81,28 @@ function callSUM(req, res) {
 }
 ////////////////////////////////
 
-server.get('/file/:name', function (req, res, next) {
-
+server.get("/file/:name", function(req, res, next) {
   var options = {
-    root: __dirname + '/images/',
-    dotfiles: 'deny',
+    root: __dirname + "/images/",
+    dotfiles: "deny",
     headers: {
-        'x-timestamp': Date.now(),
-        'x-sent': true
+      "x-timestamp": Date.now(),
+      "x-sent": true
     }
   };
 
   var fileName = req.params.name;
-  res.sendFile(fileName, options, function (err) {
+  res.sendFile(fileName, options, function(err) {
     if (err) {
       next(err);
     } else {
-      console.log('Sent:', fileName);
+      console.log("Sent:", fileName);
     }
   });
-
 });
 
+server.get("/", async (req, res) => {
+  res.status(200).json({ api: "NBA" });
+});
 
 module.exports = server;
